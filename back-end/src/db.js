686b7a57b8b342825e3c6e01d2af5e8e5022d48a
OmarMWarraich <earthling986@gmail.com>
2023-06-08 14:01:18 +0500

    fix: Env Vars

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,9 +1,10 @@
 import { MongoClient } from 'mongodb';
+require('dotenv').config();
 
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb+srv://process.env.REACT_APP_USERNAME:process.env.REACT_APP_PASSWORD@cluster0.zm7bgjy.mongodb.net/', {
+    client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zm7bgjy.mongodb.net/`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
