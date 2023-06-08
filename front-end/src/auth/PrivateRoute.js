53d0f5545ea51907ca2a86c5ac46b204c6945392
OmarMWarraich <earthling986@gmail.com>
2023-06-08 23:47:15 +0500

    feat: Routes Login/Signup jwt

--- front-end/src/auth/PrivateRoute.js
+++ front-end/src/auth/PrivateRoute.js
@@ -4,6 +4,16 @@ export const PrivateRoute = ( props ) => {
 
     const user = null;
 
+    /**
+     * 
+     * if user is not logged in, redirect to login page
+     * We're using the Navigate component to redirect the user to the login page
+     * and we're passing the current path as the state so that we can redirect the user
+     * back to the current page after they log in
+     * Navigate component is imported from react-router-dom
+     * 
+     */
+
     return user ? (
         <Route {...props} />
     ) : (
